3d1c45ebf3c83a71c804124794678bbf8423654a
Khadija_Yusuf <khadija.cfk@gmail.com>
2018-06-02 11:34:52 +0300

    me commit

---
+++ index.py
@@ -0,0 +1,17 @@
+list 1 =[6,5,4,3,]
+print (list 1)
+
+list 2 =["fatna","doro","sherry"]
+print (list 2)
+
+list 3 =["doro","dondas",5,3]
+print (list 3)
+
+tuple = ("West-Africa","England",4,9)
+
+first_list = [6,4,"fatna","doro","sherry",5,3]
+first_set = (first_list)
+print (first_set)
+
+dict{"name":"sherry","age":16,"address":"England"}
+print (dict)
